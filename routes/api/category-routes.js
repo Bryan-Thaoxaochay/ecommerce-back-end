855d2f05b8090020f4c300aa4953a3fffbf22a2b
LittleMagician <bthaoxaochay@gmail.com>
2021-03-17 17:21:45 -0500

    All category routes work.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk({
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product}],
     });
     if (!categoryData) {
@@ -37,9 +37,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const newCategory = await Category.create({
-      category_id: req.body.category_id
-    });
+    const newCategory = await Category.create(
+      req.body
+      // {category_id: req.body.category_id}
+    );
     res.status(200).json(newCategory);
   } catch (err) {
     res.status(500).json(err);
@@ -49,7 +50,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const updateCategory = await Category.update({
+    const updateCategory = await Category.update(req.body, {
       where: {
         id: req.params.id
       }
